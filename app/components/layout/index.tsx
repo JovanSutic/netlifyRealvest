6d1b18cb29b9669567deb06a47fb5c66ca47c659
unknown <josutic.sutic@gmail.com>
2024-04-22 21:57:56 +0200

    feat(86by8cwpj): create grid maps

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,4 +1,60 @@
-import { createLayoutClasses } from "../../utils/numbers";
+const columnsMap: Record<number, string> = {
+  "1": "grid-cols-1",
+  "2": "grid-cols-2",
+  "3": "grid-cols-3",
+  "4": "grid-cols-4",
+  "5": "grid-cols-5",
+  "6": "grid-cols-6",
+  "7": "grid-cols-7",
+  "8": "grid-cols-8",
+  "9": "grid-cols-9",
+  "10": "grid-cols-10",
+  "11": "grid-cols-11",
+  "12": "grid-cols-12",
+};
+
+const gapsMap: Record<number, string> = {
+  "1": "gap-1",
+  "2": "gap-2",
+  "3": "gap-3",
+  "4": "gap-4",
+  "5": "gap-5",
+  "6": "gap-6",
+  "7": "gap-7",
+  "8": "gap-8",
+  "9": "gap-9",
+  "10": "gap-10",
+  "11": "gap-11",
+  "12": "gap-12",
+};
+
+const spanMap: Record<number, string> = {
+  "1": "col-span-1",
+  "2": "col-span-2",
+  "3": "col-span-3",
+  "4": "col-span-4",
+  "5": "col-span-5",
+  "6": "col-span-6",
+  "7": "col-span-7",
+  "8": "col-span-8",
+  "9": "col-span-9",
+  "10": "col-span-10",
+  "11": "col-span-11",
+  "12": "col-span-12",
+};
+
+const startMap: Record<number, string> = {
+  "1": "col-start-1",
+  "2": "col-start-2",
+  "3": "col-start-3",
+  "4": "col-start-4",
+  "5": "col-start-5",
+  "6": "col-start-6",
+  "7": "col-start-7",
+  "8": "col-start-8",
+  "9": "col-start-9",
+  "10": "col-start-10",
+};
 
 export const WidgetWrapper = ({
   children,
@@ -21,9 +77,6 @@ export const TLine = ({
   columns: number;
   gap?: number;
 }) => {
-  const columnsMap = createLayoutClasses(12, "grid");
-  const gapsMap = createLayoutClasses(12, "gap");
-
   return (
     <div className={`grid ${columnsMap[columns]} grid-rows-1 ${gapsMap[gap]}`}>
       {children}
@@ -40,9 +93,6 @@ export const TColumn = ({
   span: number;
   start?: number;
 }) => {
-  const spanMap = createLayoutClasses(12, "span");
-  const startMap = createLayoutClasses(10, "start");
-
   return (
     <div
       className={`${spanMap[span]} ${startMap[start]} flex flex-col relative box-border px-1 py-0`}
