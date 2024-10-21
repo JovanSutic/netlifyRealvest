a20423460b1596b3a65cc9f8fcbaaf50c43aca3e
unknown <josutic.sutic@gmail.com>
2024-10-21 22:22:51 +0200

    feat(86c0qx9nk): speed fixes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -246,6 +246,8 @@ export default function Index() {
                   <img
                     src="https://belgradeepass.com/uploads/images/b_adsiz-tasarim-21-1-.webp"
                     alt="Belgrade cityscape"
+                    width="100%"
+                    height="auto"
                   />
                 </picture>
                 <div className="relative md:absolute top-0 md:top-10 w-full flex flex-col items-center mb-4 md:mb-0">
