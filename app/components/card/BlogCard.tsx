329efb7b871cc92d51ff88abfb3235dd63797b37
unknown <josutic.sutic@gmail.com>
2024-11-01 19:30:33 +0100

    feat(86c0ugvhy): change sitemap + some fixes

--- app/components/card/BlogCard.tsx
+++ app/components/card/BlogCard.tsx
@@ -7,37 +7,38 @@ const BlogCard = ({
   blog,
   lang,
   isHome = false,
+  isBorder = true,
 }: {
   blog: Blog;
   lang: LangType;
   isHome?: boolean;
+  isBorder?: boolean;
 }) => {
   return (
     <div
       className={`w-full flex flex-col items-center md:flex-row p-2  ${
-        !isHome && "border-b-[1px] border-gray-300"
+        !isHome && isBorder && "border-b-[1px] border-gray-300"
       }`}
     >
       <Link to={`/blog/${blog.slug}/?lang=${lang}`}>
         <div
           //@ts-expect-error ts-ignore
           style={{ "--image-url": `url(${blog.media_link})` }}
-          className={`w-[220px] h-[140px] mx-auto bg-[image:var(--image-url)] bg-cover center rounded`}
+          className={`w-[220px] h-[140px] mx-auto bg-[image:var(--image-url)] bg-cover center rounded-md`}
         ></div>
       </Link>
-      <div className="flex-1 md:pl-2 lg:pl-3 mt-2 lg:mt-0">
-        <Link
-          to={`/blog/${blog.slug}/?lang=${lang}`}
-          className={`text-lg ${
-            isHome ? "lg:text-xl" : "lg:text-2xl"
-          } font-semibold text-black hover:underline`}
-        >
-          {blog.name}
+      <div className="w-full flex flex-col self-start md:pl-2 lg:pl-3">
+        <Link to={`/blog/${blog.slug}/?lang=${lang}`}>
+          <span
+            className={`block text-center md:text-left text-md lg:text-lg font-semibold text-black hover:underline`}
+          >
+            {blog.name}
+          </span>
         </Link>
-        <p className="text-sm lg:text-[16px] font-regular text-gray-500 mt-2">
+        <p className="text-center md:text-left text-sm lg:text-[15px] font-regular text-gray-500 mt-2">
           {blog.description}
         </p>
-        <p className="text-[14px] font-semibold text-gray-500 mt-4">
+        <p className="text-center md:text-left text-[14px] font-semibold text-gray-500 mt-2">
           {formatDate(blog.date_created, lang)}
         </p>
       </div>
