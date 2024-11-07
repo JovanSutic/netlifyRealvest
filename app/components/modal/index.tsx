67c547250fe97dd672983d01eec2625cb274fb21
unknown <josutic.sutic@gmail.com>
2024-11-07 17:39:16 +0100

    feat(86c0xjq78): add price and rent manipulation in opportunity

--- app/components/modal/index.tsx
+++ app/components/modal/index.tsx
@@ -1,16 +1,22 @@
 const Modal = ({
   children,
   open,
+  width = "regular",
 }: {
   children: JSX.Element;
   open: boolean;
+  width?: string;
 }) => {
   if (open) {
     return (
       <div className="fixed top-0 left-0 bg-black bg-opacity-40 w-screen h-screen">
         <div className="w-full h-screen flex flex-col align-center justify-center">
           <div className="flex self-center p-4">
-            <div className="bg-white rounded-xl w-full md:w-[420px] xl:w-[560px] px-4 py-6">
+            <div
+              className={`bg-white rounded-xl w-full md:w-[420px] ${
+                width === "large" ? "xl:w-[960px]" : "xl:w-[560px]"
+              } px-4 py-6`}
+            >
               {children}
             </div>
           </div>
